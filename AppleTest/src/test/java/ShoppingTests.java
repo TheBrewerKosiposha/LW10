6217f325bb05f647ecc74466f734775d0764f012
TheBrewerKosiposha <87241701+TheBrewerKosiposha@users.noreply.github.com>
2023-01-22 16:30:48 +0300

    Add files via upload

--- AppleTest/src/test/java/ShoppingTests.java
+++ AppleTest/src/test/java/ShoppingTests.java
@@ -1,48 +1,27 @@
 import org.testng.annotations.Test;
-import page.*;
+import page.NewPerexod;
+import page.ProductPage;
+import page.ShoesPage;
 
 public class ShoppingTests extends CommonConditions
 
 {
-    /*
-@Test
-public void buyingTwoIdenticalPairs()  {
-      new ShoesPage(driver)
-            .openPage()
-              .chooseTheFirstPair();
-  }
- */
 
+    @Test
+    public void buyingTwoIdenticalPairs()  {
+       new NewPerexod(driver)
+               .openPage();
+
+
+    }
+    /*
     @Test
     public void Whhat()
     {
         new ProductPage(driver)
                 .openPage()
-        .addToBag()
-        .addToBagTwo();
-
-    }
-
-    /*
-    @Test
-    public void PerexodBRazdelMenUObratnoZad()
-    {
-        new PerexodBRazdelMenUObratno(driver).openPage();
+                .addToBag();
     }
 */
-    /*
-    @Test
-    public void Perexod()
-    {
-        new Perexod(driver).openPage();
-    }
-    */
-
-     @Test
-    public void PerxodKNew()
-     {
-         new PerxodKNew(driver).openPage();
-     }
-
 
 }
